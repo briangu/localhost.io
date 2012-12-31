ee4a9085e9e2c67ac852f7c0e613918971aef9c4
Brian Guarraci <brian@ops5.com>
2012-12-31 10:49:56 -0800

    update

--- src/main/scala/io/localhost/Main.scala
+++ src/main/scala/io/localhost/Main.scala
@@ -1,12 +1,36 @@
 package io.localhost
 
-import io.viper.common.{StaticFileContentInfoProviderFactory, NestServer, ViperServer}
+import io.viper.common._
+import jpbetz.cli._
 
 
 object Main {
   def main(args: Array[String]) {
+    val app: CommandSet = new CommandSet("localhost")
+    app.addSubCommand(classOf[DirectoryServerCommand])
+    app.addSubCommand(classOf[VirtualServerCommand])
+    app.invoke(args)
+  }
+}
+
+@SubCommand(name = "dir", description = "Directory server")
+class DirectoryServerCommand extends Command {
+  @Arg(name = "path", optional = false) var _path: String = null
+  @Opt(opt = "r", longOpt = "port", description = "port", required = false) private var _port: Number = 8080
+
+  def exec(commandLine: CommandContext) {
     StaticFileContentInfoProviderFactory.enableCache(false)
-    NestServer.run(8080, new ViperServer(args(0)))
+    NestServer.run(_port.intValue(), new ViperServer(_path))
+  }
+}
+
+@SubCommand(name = "jar", description = "Load VirtualServers from jars")
+class VirtualServerCommand extends Command {
+  @Arg(name = "path", optional = false) var _path: String = null
+  @Opt(opt = "r", longOpt = "port", description = "port", required = false) private var _port: Number = 8080
+
+  def exec(commandLine: CommandContext) {
+    new DynamicContainer(_port.intValue(), _path).run
   }
 }
 
